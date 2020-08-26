5988340fe25a33f8d2d3f086146c94cb749d5256
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-26 22:01:33 +0300

    Initial Commit

---
+++ CTPickerSwiftUI/CTPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTPickerSwiftUIApp.swift
+//  CTPickerSwiftUI
+//
+//  Created by Ramill Ibragimov on 26.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct CTPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
